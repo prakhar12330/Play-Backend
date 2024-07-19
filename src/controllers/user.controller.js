e36ce274af9ba775269a0eaacf92fc7882846631
Prakhar Singh <prakhar12330@gmail.com>
2024-07-19 21:45:03 +0530

    resister controller : bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
       throw new apiError(400, "All fields are required");
    }
    //(3)
-   const existedUser=User.findOne({
+   const existedUser=await User.findOne({
       $or:[{username},{email}]
    })
    if(existedUser){
@@ -37,7 +37,12 @@ const registerUser = asyncHandler(async (req, res) => {
    //(4)
    //Optional Chaining
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   let coverImageLocalPath;
+   if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0 ){
+      coverImageLocalPath = req.files?.coverImage[0]?.path;
+   }
+
    if(!avatarLocalPath){
       throw new apiError(400,"Avatar file is required")
    }
